f0fc125c9a5459fa43c03025f86326539574b274
Joseph Calla <josephestanislao9@gmail.com>
2022-10-25 16:44:34 -0500

    Initial Commit

---
+++ ProjectSetup/ProjectSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSetupApp.swift
+//  ProjectSetup
+//
+//  Created by Joseph Estanislao Calla Moreyra on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
