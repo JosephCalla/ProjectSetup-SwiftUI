3ca46e1e4638853ccdaffc24a44fe0f698721607
Joseph Calla <josephestanislao9@gmail.com>
2022-10-25 16:53:38 -0500

    NOTICKET: SwiftUI previews

--- ProjectSetup/ContentView.swift
+++ ProjectSetup/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("We love previews!")
         }
         .padding()
     }
@@ -21,6 +21,13 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+            
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+        
     }
 }
